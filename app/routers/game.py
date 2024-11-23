b39b8e5740ae73404492c42966b51219304ea58f
caio.batista <caio.araujo@edu.pucrs.br>
2024-11-23 09:52:36 -0300

    feat: include playing against the network

--- app/routers/game.py
+++ app/routers/game.py
@@ -1,5 +1,6 @@
 from flask import jsonify
 from pydantic import ValidationError
+from app.models.neural_network import NeuralNetwork
 from models.game_state import GameState
 from app.algorithm.minimax import Minimax
 from models.board import Board
@@ -10,8 +11,9 @@ api = Namespace('game', description='Operações com o jogo da velha')
 
 checker = BoardChecker()
 minimax = Minimax()
+network = NeuralNetwork().load_network_from_file('PATH_TO_FILE')
 
-status_model = api.model('Board', {
+minimax_model = api.model('Board', {
     'board': fields.List(fields.List(fields.String, required=True), required=True, description='Board data in JSON format', example=[
         ['x', 'b', 'b'],
         ['b', 'o', 'b'],
@@ -20,9 +22,9 @@ status_model = api.model('Board', {
     'difficulty': fields.String(required=True, description='Game`s difficulty', example='hard')
 })
 
-@api.route('/status')
-class Status(Resource):
-    @api.expect(status_model)
+@api.route('/minimax')
+class PlayMinimax(Resource):
+    @api.expect(minimax_model)
     def post(self):
         args = api.payload
         board_data = args['board']
@@ -43,3 +45,32 @@ class Status(Resource):
         return jsonify({"status": status.to_string(), 
                         "next_move": next_move,
                         "used_minimax": used_minimax})
+
+network_model = api.model('Board', {
+    'board': fields.List(fields.List(fields.String, required=True), required=True, description='Board data in JSON format', example=[
+        ['x', 'b', 'b'],
+        ['b', 'o', 'b'],
+        ['b', 'b', 'x']
+    ])
+})
+
+@api.route('/network')
+class PlayNetwork(Resource):
+    @api.expect(network_model)
+    def post(self):
+        args = api.payload
+        board_data = args['board']
+
+        try:
+            board = Board(board=board_data)
+        except ValidationError as e:
+            return jsonify(e.errors()), 400
+
+        status = checker.check_status(board.board)
+        next_move = None
+
+        if(status == GameState.NOT_OVER):
+            next_move  = network.find_next_move(board.board)
+
+        return jsonify({"status": status.to_string(), 
+                        "next_move": next_move})
